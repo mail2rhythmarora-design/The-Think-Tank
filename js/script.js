3827b88c6b90233bc176f2531ce21ccf30718cf8
Rhythm Arora <mail2rhythmarora@gmail.com>
2026-03-31 19:03:05 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const checkBtn = document.getElementById("checkBtn");
 const newsInput = document.getElementById("newsInput");
 const resultBox = document.getElementById("result");
 
-const API_KEY = "PASTE_YOUR_API_KEY_HERE";
+const API_KEY = "AIzaSyBoDCq53DkFWiibWmRqiqdEhfHwJDn9eiQ";
 
 checkBtn.addEventListener("click", async () => {
     const text = newsInput.value.trim();
