5a3670eb4bf523b5d71efd8e1b10644c2cae727f
Rhythm Arora <mail2rhythmarora@gmail.com>
2026-03-31 18:20:17 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,35 +1,48 @@
 const checkBtn = document.getElementById("checkBtn");
 const newsInput = document.getElementById("newsInput");
 const resultBox = document.getElementById("result");
+const confidenceBox = document.getElementById("confidence");
 
 checkBtn.addEventListener("click", () => {
   const text = newsInput.value.trim();
 
   if (text === "") {
-    resultBox.innerHTML = "⚠️ Please enter some news text!";
+    resultBox.innerHTML = "⚠️ Please enter some news!";
     resultBox.style.color = "orange";
+    confidenceBox.innerHTML = "";
     return;
   }
 
   resultBox.innerHTML = "Analyzing...";
   resultBox.style.color = "white";
+  confidenceBox.innerHTML = "";
 
   setTimeout(() => {
-    const fakeWords = ["shocking", "click here", "guaranteed", "secret", "breaking", "unbelievable"];
-    let isFake = false;
+    const fakeWords = [
+      "shocking", "breaking", "unbelievable",
+      "click here", "guaranteed", "secret", "100% true"
+    ];
+
+    let score = 0;
 
     fakeWords.forEach(word => {
       if (text.toLowerCase().includes(word)) {
-        isFake = true;
+        score += 15;
       }
     });
 
-    if (isFake) {
-      resultBox.innerHTML = "🚨 This news looks FAKE!";
+    if (score > 80) score = 85;
+
+    if (score >= 50) {
+      resultBox.innerHTML = "🚨 FAKE NEWS DETECTED!";
       resultBox.style.color = "red";
+      confidenceBox.innerHTML = `Confidence: ${score}%`;
     } else {
-      resultBox.innerHTML = "✅ This news looks REAL!";
+      let realScore = 100 - score;
+      resultBox.innerHTML = "✅ NEWS LOOKS REAL";
       resultBox.style.color = "lightgreen";
+      confidenceBox.innerHTML = `Confidence: ${realScore}%`;
     }
-  }, 1500);
+  }, 1200);
 });
+ 
